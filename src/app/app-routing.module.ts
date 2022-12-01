004e5ede48af4935ca76012a5042e5c422047da3
LGyorgy <l.gyorgy90@gmail.com>
2022-12-01 12:46:59 +0100

    Remove unused import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryCardDisplayComponent } from './components/category-card-display/category-card-display.component';
-import { MealDetailsComponent } from './components/meal-details/meal-details.component';
 import { CategoriesRouteComponent } from './routes/categories-route/categories-route.component';
 import { MealRouteComponent } from './routes/meal-route/meal-route.component';
 
