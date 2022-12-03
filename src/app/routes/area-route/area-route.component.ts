b74bbba595f02928f072a6b15208cf120b32b06a
LGyorgy <l.gyorgy90@gmail.com>
2022-12-03 12:47:28 +0100

    Update area page to display meals

--- src/app/routes/area-route/area-route.component.ts
+++ src/app/routes/area-route/area-route.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MealService } from 'src/app/services/meal.service';
+import { SimpleMeal } from 'src/interfaces/simple-meal';
 
 @Component({
   selector: 'app-area-route',
   templateUrl: './area-route.component.html'
 })
 export class AreaRouteComponent {
+  area: string | undefined;
+  meals: SimpleMeal[] | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private mealService: MealService
+  ) {}
+
+  ngOnInit() {
+    this.area = String(this.route.snapshot.paramMap.get('areaName'));
+    this.mealService.getMealsByArea(this.area).subscribe(
+      meals => this.meals = meals
+    )
+  }
 }
